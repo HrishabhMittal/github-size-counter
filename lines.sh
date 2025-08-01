281796acb800d3e95e45d712050cc4e7b962975a
HrishabhMittal <hrishabhmittal.hm@gmail.com>
2025-08-02 01:49:36 +0530

    initial commit

---
+++ lines.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# wouldnt expect this to appear but it does so yea
+END_PAGE="doesn’t have any public repositories yet"
+
+
+EXTENSIONS=(html css cpp py lua c js ts sh md)
+
+find_name_args=()
+for ext in "${EXTENSIONS[@]}"; do
+  if [[ ${#find_name_args[@]} -gt 0 ]]; then
+    find_name_args+=( -o )
+  fi
+  find_name_args+=( -name "*.${ext}" )
+done
+
+find_args=( \( "${find_name_args[@]}" \) -type f )
+
+PAGE="1"
+USERNAME="HrishabhMittal"
+LINK="https://github.com"
+while [[ "$PAGE" != "-1" ]]; do 
+    TMP_FILE="$(mktemp)"
+    curl -s "$LINK/$USERNAME?page=$PAGE&tab=repositories" --output "$TMP_FILE"
+    if grep -q "$END_PAGE" "$TMP_FILE"; then
+        rm "$TMP_FILE"
+        break
+    fi
+    ((PAGE++))
+    grep -oP 'href="\K[^"]+' "$TMP_FILE" | grep -P "^/$USERNAME/[^/]+$"
+    rm "$TMP_FILE"
+    sleep 1 # just to be nice to github :)
+done | while read -r line; do 
+    REPO_LINK="$LINK$line"
+    git clone --quiet "$REPO_LINK" repo
+    COUNT=$(find repo "${find_args[@]}" -exec cat {} + | wc -l)
+    echo "$REPO_LINK: $COUNT"
+    rm repo -rf
+done
